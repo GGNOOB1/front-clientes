f1a44f951923eea292f0cfdc858290bf3bc553ab
Geraldo Vicente <geraldovicente987@gmail.com>
2023-09-26 15:47:20 -0300

    Fix: arrumando erro de escrita

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -366,7 +366,7 @@ export default function Cadastro() {
                 type="submit"
                 className="globalButton flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {isLoading ? "Carregando.." : "Entrar"}
+                {isLoading ? "Carregando.." : "Cadastrar"}
               </button>
             </div>
           </form>
