3e9fd5e7dd7b495f453b80f3a831f01c2e50467e
Geraldo Vicente <geraldovicente987@gmail.com>
2023-10-10 13:26:36 -0300

    Add: Links das apis e customError

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -12,3 +12,21 @@ export const perfil = {
   api_local: "http://localhost:3001/api/v1/clientes",
   api_online: "https://api-clientes-fatec-v1.onrender.com/api/v1/clientes",
 };
+
+export const recuperarSenhaUrl = {
+  api_local: "http://localhost:3001/api/v1/auth/recuperarSenha",
+  api_online:
+    "https://api-clientes-fatec-v1.onrender.com/api/v1/auth/recuperarSenha",
+};
+
+export const verificarTokenRecuperarSenha = {
+  api_local: "http://localhost:3001/api/v1/auth/reset-senha",
+  api_online:
+    "https://api-clientes-fatec-v1.onrender.com/api/v1/auth/reset-senha",
+};
+
+export const atualizarSenha = {
+  api_local: "http://localhost:3001/api/v1/auth/nova-senha",
+  api_online:
+    "https://api-clientes-fatec-v1.onrender.com/api/v1/auth/nova-senha",
+};
